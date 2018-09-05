f6b338d3fe5ea01ee165b2f3c8a47a3db459ef64
Alexey Anosov <a.anosov85@gmail.com>
2018-09-05 15:07:26 +0500

    fixed method

--- tests.py
+++ tests.py
@@ -9,12 +9,26 @@ class TestUM(unittest.TestCase):
         self.ftp = ftplib.FTP('')
         self.ftp.connect('localhost', 2121)
         self.ftp.login("user", "12345")
+        self.file_to_download = ""   #<--- указать имя файла, который требуется загрузить с сервера
+        self.savepath = ""           #<--- указать путь, где будет записан загружаемый файл
+        self.file_to_upload = ""     #<--- указать файл, который требуется выгрузить на сервер
 
+    def test_download(self):
+        self.assertTrue(ftpm.download_file(self.ftp, self.file_to_download, self.savepath))
+
+    def test_upload(self):
+        self.assertTrue(ftpm.upload_file(self.ftp, self.file_to_upload))
+
+
+
+    ''' Данная конфигурация использовалась для запуска тестов на локальном компьютере.    
+    
     def test_download(self):
         self.assertTrue(ftpm.download_file(self.ftp, 'bf.txt', '/home/alexey2/testfold/bf.txt'))
 
     def test_upload(self):
         self.assertTrue(ftpm.upload_file(self.ftp, '/home/alexey2/testfold/bf.txt'))
+    '''
 
     def tearDown(self):
         self.ftp.quit()
